cbfe184c0ab173efb1c615a428b3bb638f48c02b
vamshi krishna <53006565+vamshikrishnafive@users.noreply.github.com>
2021-09-19 22:14:10 +0530

    one

---
+++ app.js
@@ -0,0 +1,19 @@
+/* Using the countries array create the following array of arrays. The country name, the first three letters of the country name and the length of the country name */
+
+// function createArrayOfArrays(array) {
+//     return array.map(value => [value, value.slice(0,3), value.length])
+// }
+// const countries = [
+//     'ALBANIA',
+//     'BOLIVIA',
+//     'CANADA',
+//     'DENMARK',
+//     'ETHIOPIA',
+//     'FINLAND',
+//     'GERMANY',
+//     'HUNGARY',
+//     'IRELAND',
+//     'JAPAN',
+//     'KENYA'
+// ]
+// console.log(createArrayOfArrays(countries))
